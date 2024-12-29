5bd9b36e301667c30fa89046cd8e169aad73d00b
Newton  Sarker <newtonsarker@gmail.com>
2024-12-29 13:12:29 +0100

    refactored code

--- src/python/single_feature_gradient_descent_regression.py
+++ src/python/single_feature_gradient_descent_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plot
 import algorithm.linear_regression.gradient_descent as gd
 
 def main():
@@ -6,9 +7,17 @@ def main():
     training_values_y = np.array([300.0, 500.0])
     w_in = 0
     b_in = 0
-    alpha = 8.0e-1
-    num_iters = 10
-    gd.gradient_descent_of_single_feature_model(training_values_x, training_values_y, w_in, b_in, alpha, num_iters)
+    alpha = 5.0e-7
+    num_iters = 9000000
+    w_final, b_final, J_hist = gd.gradient_descent_of_single_feature_model(training_values_x, training_values_y, w_in, b_in, alpha, num_iters)
+
+    fig, (ax1, ax2) = plot.subplots(1, 2, constrained_layout=True, figsize=(12, 4))
+    ax1.plot(J_hist)
+    ax2.plot(100 + np.arange(len(J_hist[100:])), J_hist[100:])
+    ax1.set_title("Cost vs. iteration");  ax2.set_title("Cost vs. iteration (tail)")
+    ax1.set_ylabel('Cost')             ;  ax2.set_ylabel('Cost')
+    ax1.set_xlabel('iteration step')   ;  ax2.set_xlabel('iteration step')
+    plot.show()
 
 if __name__ == "__main__":
     main()
